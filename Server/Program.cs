343e37b8f04985ee06ee6e72da859d3d27e65d9e
Thanasarn Kittimethee <tnasan@hotmail.com>
2017-12-17 22:20:43 +0700

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listen = new TcpListener(System.Net.IPAddress.Loopback, 500);
+            listen.Start();
+
+            byte[] buffer = new byte[65535];
+            while (true)
+            {
+                var client = listen.AcceptTcpClient();
+                var stream = client.GetStream();
+
+                int readLength;
+                while ((readLength = stream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    //Thread.Sleep(300);
+                    var data = Encoding.ASCII.GetString(buffer, 0, readLength);
+                    Debug.WriteLine("Server: " + data);
+                    if (data.CompareTo("Hello World") == 0)
+                    {
+                        var dataToSend = Encoding.ASCII.GetBytes("Hello Too!");
+                        stream.Write(dataToSend, 0, dataToSend.Length);
+                    }
+                }
+
+                client.Close();
+            }
+        }
+    }
+}
---
+++ TcpClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient(System.Net.IPAddress.Loopback.ToString(), 500);
+            var stream = client.GetStream();
+            stream.ReadTimeout = 500;
+
+            var sendText = "Hello World";
+            var encodedText = Encoding.ASCII.GetBytes(sendText);
+            stream.Write(encodedText, 0, encodedText.Length);
+
+            var buffer = new byte[65535];
+            do
+            {
+                try
+                {
+                    var readCount = stream.Read(buffer, 0, 65535);
+                    Debug.WriteLine("Client: " + Encoding.ASCII.GetString(buffer, 0, readCount));
+                }
+                catch (Exception e)
+                {
+                    break;
+                }
+            } while (true);
+
+            Console.WriteLine("Finish!");
+            Console.WriteLine($"Connection: {client.Connected}");
+
+            Console.ReadKey();
+        }
+    }
+}
